b82890bb77b4175e973390a02173fce13fe64759
Matt Jeffery <matt.jeffery@arkansas.gov>
2020-04-14 11:13:00 -0500

    Pull in all my normal functions and a few snippets

--- skel/functions.sh
+++ skel/functions.sh
@@ -1,11 +1,218 @@
-#!/bin/bash
-# ------------------------------------------------------------------------
-# $Id: shell.sh 32 2011-03-10 03:53:55Z mjeffe $
-# 
-# Usefull shell code snippets
-# NOTE: see perl.pl for perl one liners that can be used in shell scripts.
-# ------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
+# Useful shell script helper functions. You should not source this directly
+# because there are a number of bare code snippets. Copy what is useful into
+# your own project and modify to suit.
+# ---------------------------------------------------------------------------
 
+
+# ==========================================================================
+#
+# This group of functions *could* be used as is
+#
+# ==========================================================================
+
+# ---------------------------------------------------------------------------
+_sigint_handler() {
+   echo "Ctrl-C pressed, aborting..."
+   _cleanup
+   exit 1
+}
+trap _sigint_handler SIGINT
+
+# ---------------------------------------------------------------------------
+_say() {
+   dt=`date | perl -ne 'chomp; print'`
+   echo "$dt: $1"
+}
+
+
+# ---------------------------------------------------------------------------
+_chkerr() {
+   if [ $1 -ne 0 ]; then
+      _say "$2 exited with error (rc=$1)"
+      #_email "ERROR: $2"
+      exit $1
+   fi
+}
+
+# ---------------------------------------------------------------------------
+_die() {
+  _say "$1"
+  exit 1
+}
+
+# ---------------------------------------------------------------------------
+_cleanup() {
+  # cleanup
+  _say "cleaning up"
+  #rm -fr $tmpdir
+}
+
+# ---------------------------------------------------------------------------
+_loadcsv() {
+  # spawn subshell so as not to polute environment with .env vars
+  (
+    f="$1"
+    base=$(basename $f .csv)
+    _say "loading $f"
+
+    source .env
+    export PGPASSWORD="$DB_PASSWORD"
+    pgloadcsv -u $DB_USERNAME -d $DB_DATABASE -h $DB_HOST --drop -c -t stg_$base $f
+    _chkerr "$?" "pgloadcsv of $f"
+  )
+}
+
+# ---------------------------------------------------------------------------
+_read_dot_env_var() {
+  if [ -z "$1" ]; then
+    _say "Need an argument of the variable name you are looking for"
+    exit 1
+  fi
+
+  ENV_VAL=$(egrep "^${1}=" .env | cut -d '=' -f 2)
+
+  echo ${ENV_VAL}
+}
+
+# ==========================================================================
+#
+# Functions in this group are not immediately useful, they are examples that
+# you can copy/modify to suit for a particular situation
+#
+# ==========================================================================
+
+# ---------------------------------------------------------------------------
+_email() {
+   _say "sending email"
+   for e in $email_recipients; do
+       #cat <<EOF | /usr/sbin/sendmail -f matt.jeffery@arkansas.gov matt.jeffery@arkansas.gov
+       cat <<EOF | /usr/bin/mailx -t
+From: matt.jeffery@arkansas.gov
+To: $e
+Subject: ERROR: GED daily gedts.sh script
+
+$1
+
+The `pwd`/gedts.sh script controls the daily update. Header notes
+in this script may be helpful for debugging.
+EOF
+
+done
+}
+
+# ---------------------------------------------------------------------------
+_runsql_example()
+{
+   sqlf=$1
+   base=`basename $sqlf .sql`
+   _say "running job: $sqlf";
+   psql -X -a -U $dbuser -d $dbname -h $dbhost -f $sqlf > logs/$base.log
+   _chkerr $? $sqlf
+}
+
+# ---------------------------------------------------------------------------
+_usage_example()
+{
+   cat <<EOF
+   Description: do something
+
+   Usage: `basename $0` [-h?] [-f filename] [-d YYYYMMDD]
+
+   Options:
+      Default is to fetch today's file and load it.  The following options
+      change this behavior.
+
+      -f filename    Skips the fetch and instead loads the local filename
+      -d YYYYMMDD    Fetches and loads file for YYYYMMDD
+      -h -?          Prints this usage message
+
+EOF
+}
+
+# ==========================================================================
+#
+# The remainder are code snippets of useful stuff
+#
+# ==========================================================================
+
+# ------------------------------------------------------------------------
+# prompt user for input
+# man bash - and search for /^SHELL BUILTIN/ then page down to 'read'
+# ------------------------------------------------------------------------
+# prompt for password
+read -sp "Your password  : " pw
+
+# prompt for non-hidden input
+read -p "Do you want to proceed? [yN] " answer
+# handle default
+if [ -z "$answer" -o "$answer" = "N" ]; then
+    _die "Ok, I'm quitting..."
+fi
+
+# ------------------------------------------------------------------------
+# return a random number between 1 and $1
+# ------------------------------------------------------------------------
+expr $RANDOM % $1 + 1
+
+# ---------------------------------------------------------------------------
+# getopts short example
+# ------------------------------------------------------------------------
+while getopts "f:d:th" opt; do
+   case $opt in
+      d) filedate=$OPTARG ;;
+      f) inputfile=$OPTARG ;;
+      t) TARBALL=TRUE ;;
+      h) _usage; exit 0 ;;
+      *) echo "invalid option... try running `basename $0` -h"; exit 1 ;;
+   esac
+done
+shift $(($OPTIND - 1))
+inputfile="$1"
+
+# required command line parameters
+if [ -z "$filedate" ]; then
+   _die "Missing required option: -d YYYMMDD"
+fi
+
+
+# ---------------------------------------------------------------------------
+# getopts long example
+# ------------------------------------------------------------------------
+LONG_ARGS_FLAGS=("completions:")
+opts=$(
+  getopt \
+    --longoptions "$(printf "%s:," "${LONG_ARGS_FLAGS[@]}")" \
+    --name "$(basename "$0")" \
+    --options "lh" \
+    -- "$@"
+)
+
+eval set --"$opts"
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+  --completions)
+    _source_completions
+    shift
+    ;;
+  -l)
+    _list_available
+    exit 0
+    ;;
+  -h)
+    _usage
+    exit 0
+    ;;
+  *)
+    echo $1
+    echo "invalid option... try running $(basename $0) -h"
+    exit 1
+    ;;
+  esac
+  shift
+  # shift $(($OPTIND - 1))
+done
 
 
 # ------------------------------------------------------------------------
@@ -15,8 +222,7 @@
 
 # this works on GNU/Linux
 str=
-while read line
-do
+while read line; do
    str="$str $line"
 done <<EOF
 `df -h`
@@ -27,27 +233,15 @@ echo $str
 # This works in ksh/Tru64
 #df -h | while read $line
 
-
 # ------------------------------------------------------------------------
-# displays the specified line from a file
+# grab the specified line from a file
 # ------------------------------------------------------------------------
-if [ ! "$1" ] || [ ! "$2" ]
+if [ "$filename" = "-" ]
 then
-   echo "Description:"
-   echo "  `basename $0` displays line_number from the input_file or - (stdin)."
-   echo "Usage: "
-   echo "   `basename $0` <line_number> <input_file|->"
-   echo
-   exit 1
-fi
-
-if [ "$2" = "-" ]
-then
-   head -n${1} | tail -n 1
+   head -n${linenum} | tail -n 1
 else
-   head -n${1} $2 | tail -n 1
+   head -n${linenum} $filename | tail -n 1
 fi
-
 
 # ------------------------------------------------------------------------
 # create a string of repeated characters
@@ -73,35 +267,6 @@ makestr 2 AV
 echo $myvar
 
 # ------------------------------------------------------------------------
-# remove blank lines from a file
-# ------------------------------------------------------------------------
-if [ ! "$1" ]
-then
-   echo "missing parameter..."
-   echo "Usage: `basename $0` source_file"
-   exit 1
-fi
-
-awk '
-
-# skip empty rows
-$0 == "" {
-   next
-}
-
-# print all non blank rows
-{
-   print
-}' $1
-
-
-# ------------------------------------------------------------------------
-# return a random number between 1 and $1
-# ------------------------------------------------------------------------
-expr $RANDOM % $1 + 1
-
-
-# ------------------------------------------------------------------------
 # Color ORACLE_SID portion of your promp RED for production SID.
 # ------------------------------------------------------------------------
 # make sure this script is in your path, preferably ~/bin
@@ -115,4 +280,7 @@ if [ "$ORACLE_SID" = "cdi" ]; then
 else
    echo "[$USER@$MYHOSTNAME..$ORACLE_SID] $PWD => "
 fi
+
+
+
 
